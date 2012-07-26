bcdeb8adfc3de9326ae84175ed0af6dc088af9e1
veka peltola <veka.peltola@ericsson.com>
2012-07-26 14:48:02 +0300

    Gamer Update gamerTest. Adds setUp function.

--- src/test/java/dojo/dependency/GamerTest.java
+++ src/test/java/dojo/dependency/GamerTest.java
@@ -6,21 +6,27 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GamerTest {
-
+	Gamer gamer;
+	
+	@Before
+	public void setUp() {
+	  this.gamer = new Gamer();
+	}
+	
 	@Test
 	 public void runPlay(){
 		//Given
-		Gamer game = new Gamer();
 		ByteArrayInputStream inputStream = new ByteArrayInputStream("1,5".getBytes());
 		System.setIn(inputStream);
 
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outputStream, true));
 		//when
-		game.play();
+		gamer.play();
 		//then
 		assertEquals("fizz buzz: " + System.getProperty("line.separator") + "1 2 Fizz 4 Buzz ",outputStream.toString());
 	}
